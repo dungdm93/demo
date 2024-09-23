cff7c865105ffe72563b065aec3c769a70d06efc
Đặng Minh Dũng <dungdm93@live.com>
2024-09-23 14:56:48 +0700

    tree

--- .github/actions/js1/index.js
+++ .github/actions/js1/index.js
@@ -1,7 +1,9 @@
 console.log(process.env);
 
-console.log("CWD");
-console.log(process.cwd())
+console.log(`CWD: ${process.cwd()}`);
+
+const currentCommand = process.argv.join(' ');
+console.log(`Current running command: ${currentCommand}`);
 
 console.log("exit 1");
 process.exit(1);
--- .github/actions/js1/post.js
+++ .github/actions/js1/post.js
@@ -1,4 +1,9 @@
 console.log(process.env);
 
+console.log(`CWD: ${process.cwd()}`);
+
+const currentCommand = process.argv.join(' ');
+console.log(`Current running command: ${currentCommand}`);
+
 // console.log("exit 1");
 // process.exit(1);
